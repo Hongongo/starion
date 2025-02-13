a73c344bdb94db77b7db4955c7c6548d855e764c
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-13 12:41:46 -0600

    triggers check permissions when provider its created

--- lib/presentation/providers/permissions/permissions_provider.dart
+++ lib/presentation/providers/permissions/permissions_provider.dart
@@ -1,73 +1,71 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-
 //state notifier provider
-final permissionsProvider = StateNotifierProvider<PermissionsNotifier, PermissionsState>((ref) {
+final permissionsProvider =
+    StateNotifierProvider<PermissionsNotifier, PermissionsState>((ref) {
   return PermissionsNotifier();
 });
 
-
 class PermissionsNotifier extends StateNotifier<PermissionsState> {
-  PermissionsNotifier(): super( PermissionsState() );
+  PermissionsNotifier() : super(PermissionsState()) {
+    checkPermissions();
+  }
 
   Future<void> checkPermissions() async {
     final permissionsArray = await Future.wait([
-        Permission.camera.status,
-        Permission.photos.status,
-        Permission.sensors.status,
-
-        Permission.location.status,
-        Permission.locationAlways.status,
-        Permission.locationWhenInUse.status,
+      Permission.camera.status,
+      Permission.photos.status,
+      Permission.sensors.status,
+      Permission.location.status,
+      Permission.locationAlways.status,
+      Permission.locationWhenInUse.status,
     ]);
 
     state = state.copyWith(
-      camera           : permissionsArray[0],
-      photoLibrary     : permissionsArray[1],
-      sensors          : permissionsArray[2],
-
-      location         : permissionsArray[3],
-      locationAlways   : permissionsArray[4],
+      camera: permissionsArray[0],
+      photoLibrary: permissionsArray[1],
+      sensors: permissionsArray[2],
+      location: permissionsArray[3],
+      locationAlways: permissionsArray[4],
       locationWhenInUse: permissionsArray[5],
     );
-
   }
 
   openSettinsScreen() {
     openAppSettings();
   }
 
-  void _checkPerssionState( PermissionStatus status ) {
-    if ( status == PermissionStatus.permanentlyDenied ) {
+  void _checkPerssionState(PermissionStatus status) {
+    if (status == PermissionStatus.permanentlyDenied) {
       openSettinsScreen();
     }
   }
 
   requestCameraAccess() async {
     final status = await Permission.camera.request();
-    state = state.copyWith( camera: status );
+    state = state.copyWith(camera: status);
 
     _checkPerssionState(status);
   }
 
   requestPhotoLibraryAccess() async {
     final status = await Permission.photos.request();
-    state = state.copyWith( photoLibrary: status );
+    state = state.copyWith(photoLibrary: status);
 
     _checkPerssionState(status);
   }
 
   requestLocationAccess() async {
     final status = await Permission.location.request();
-    state = state.copyWith( location: status );
+    state = state.copyWith(location: status);
 
     _checkPerssionState(status);
   }
 
   requestSensorsAccess() async {
     final status = await Permission.sensors.request();
-    state = state.copyWith( sensors: status );
+    state = state.copyWith(sensors: status);
 
     _checkPerssionState(status);
   }
@@ -83,11 +81,11 @@ class PermissionsState {
   final PermissionStatus locationWhenInUse;
 
   PermissionsState({
-    this.camera            = PermissionStatus.denied, 
-    this.photoLibrary      = PermissionStatus.denied, 
-    this.sensors           = PermissionStatus.denied, 
-    this.location          = PermissionStatus.denied, 
-    this.locationAlways    = PermissionStatus.denied, 
+    this.camera = PermissionStatus.denied,
+    this.photoLibrary = PermissionStatus.denied,
+    this.sensors = PermissionStatus.denied,
+    this.location = PermissionStatus.denied,
+    this.locationAlways = PermissionStatus.denied,
     this.locationWhenInUse = PermissionStatus.denied,
   });
 
@@ -122,12 +120,13 @@ class PermissionsState {
     PermissionStatus? location,
     PermissionStatus? locationAlways,
     PermissionStatus? locationWhenInUse,
-  }) => PermissionsState(
-    camera           : camera ?? this.camera,
-    photoLibrary     : photoLibrary ?? this.photoLibrary,
-    sensors          : sensors ?? this.sensors,
-    location         : location ?? this.location,
-    locationAlways   : locationAlways ?? this.locationAlways,
-    locationWhenInUse: locationWhenInUse ?? this.locationWhenInUse,
-  );
+  }) =>
+      PermissionsState(
+        camera: camera ?? this.camera,
+        photoLibrary: photoLibrary ?? this.photoLibrary,
+        sensors: sensors ?? this.sensors,
+        location: location ?? this.location,
+        locationAlways: locationAlways ?? this.locationAlways,
+        locationWhenInUse: locationWhenInUse ?? this.locationWhenInUse,
+      );
 }
