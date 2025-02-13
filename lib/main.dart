cd5871db09be08a47e23e7d051e061e0b2acea04
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-13 11:45:42 -0600

    refactor on main.dart to listen life cycle changes on app

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,43 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print('State: $state');
+    /*
+      resumed - background to foreground
+      inactive - no longer focused
+      paused - in background
+      detached - destroyed
+    */
+    super.didChangeAppLifecycleState(state);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
+    return MaterialApp.router(
       routerConfig: router,
       theme: AppTheme().getTheme(),
       debugShowCheckedModeBanner: false,
