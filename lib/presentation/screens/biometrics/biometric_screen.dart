3deab3d914d1023288d7c1d0e8b61e66fb6d490c
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-14 16:55:29 -0600

    checks if user can use biometrics

--- lib/presentation/screens/biometrics/biometric_screen.dart
+++ lib/presentation/screens/biometrics/biometric_screen.dart
@@ -1,46 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../providers/providers.dart';
+
 class BiometricScreen extends ConsumerWidget {
-  
   const BiometricScreen({super.key});
 
   @override
-  Widget build(BuildContext context, ref ) {
-
-    // final canCheckBiometrics = ref.watch(canCheckBiometricsProvider);
+  Widget build(BuildContext context, ref) {
+    final canCheckBiometrics = ref.watch(canCheckBiometricsProvider);
     // final localAuthState = ref.watch( localAuthProvider );
 
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Biometric Screen')
-      ),
+      appBar: AppBar(title: const Text('Biometric Screen')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            FilledButton.tonal(
+              onPressed: () {
+                //  ref.read( localAuthProvider.notifier )
+                //   .authenticateUser();
+              },
+              child: const Text('Autenticar'),
+            ),
 
-            FilledButton.tonal(onPressed: (){
-
-              //  ref.read( localAuthProvider.notifier )
-              //   .authenticateUser();
-
-            }, child: const Text('Autenticar')),
-
-            
-            // canCheckBiometrics.when(
-            //   data: ( canCheck ) => Text('Puede revisar biométricos: $canCheck'), 
-            //   error: (error, _ ) => Text('Error: $error'), 
-            //   loading: () => const CircularProgressIndicator()
-            // ),
-            
-
+            canCheckBiometrics.when(
+                data: (canCheck) =>
+                    Text('Puede revisar biométricos: $canCheck'),
+                error: (error, _) => Text('Error: $error'),
+                loading: () => const CircularProgressIndicator()),
 
             const SizedBox(height: 40),
-            const Text('Estado del biométrico', style: TextStyle( fontSize: 30 )),
+            const Text('Estado del biométrico', style: TextStyle(fontSize: 30)),
             // Text('Estado $localAuthState', style: const TextStyle( fontSize: 20 )),
-
           ],
         ),
       ),
