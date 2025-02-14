2213ff7c6237fc625b36fe5fa356b1334bf486e2
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-13 23:43:26 -0600

    added accelerometer provider and screen

--- lib/presentation/screens/sensors/accelerometer_screen.dart
+++ lib/presentation/screens/sensors/accelerometer_screen.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:miscelaneos/presentation/providers/providers.dart';
+
+import '../../widgets/widgets.dart';
+import '../../providers/providers.dart';
 
 class AccelerometerScreen extends ConsumerWidget {
   const AccelerometerScreen({super.key});
 
   @override
   Widget build(BuildContext context, ref ) {
-
-    //final accelerometer$ = ref.watch( accelerometerUserProvider );
+    final accelerometer$ = ref.watch( accelerometerUserProvider );
+    // final accelerometer$ = ref.watch( accelerometerGravityProvider );
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Acelerómetro'),
       ),
-      // body: Center(
-      //   child: accelerometer$.when(
-      //     data: (value) => Text(
-      //       value.toString(),
-      //       style: const TextStyle(fontSize: 30),
-      //     ), 
-      //     error: (error, stackTrace) => Text('$error'), 
-      //     loading: () => const CircularProgressIndicator()
-      //   ),
-      // ),
+      body: Center(
+        child: accelerometer$.when(
+          data: (value) => Text(
+            value.toString(),
+            style: const TextStyle(fontSize: 30),
+          ), 
+          error: (error, stackTrace) => const SensorError(), 
+          loading: () => const CircularProgressIndicator()
+        ),
+      ),
     );
     
   }
