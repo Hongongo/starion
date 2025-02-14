d2c1637cd0d088496e859dfde12289b4757ef830
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-14 00:03:08 -0600

    added gyroscope ball screen

--- lib/presentation/screens/sensors/gyroscope_ball_screen.dart
+++ lib/presentation/screens/sensors/gyroscope_ball_screen.dart
@@ -1,74 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:miscelaneos/presentation/providers/providers.dart';
+
+import '../../providers/providers.dart';
+import '../../widgets/widgets.dart';
 
 class GyroscopeBallScreen extends ConsumerWidget {
   const GyroscopeBallScreen({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
-
-    // final gyroscope$ = ref.watch( gyroscopeProvider );
+    final gyroscope$ = ref.watch(gyroscopeProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Giroscópio'),
-      ),
-      // body: SizedBox.expand(
-      //   child: gyroscope$.when(
-      //     data: (value) => MovingBall(x: value.x, y: value.y), 
-      //     error: (error, stackTrace) => Text('$error'), 
-      //     loading: () => const CircularProgressIndicator(),
-      //   ),
-      // )
-    );
-    
+        appBar: AppBar(
+          title: const Text('Giroscópio'),
+        ),
+        body: SizedBox.expand(
+          child: gyroscope$.when(
+            data: (value) => MovingBall(x: value.x, y: value.y),
+            error: (error, stackTrace) => const SensorError(),
+            loading: () => const CircularProgressIndicator(),
+          ),
+        ));
   }
 }
 
-
 class MovingBall extends StatelessWidget {
-  
   final double x;
   final double y;
-  
+
   const MovingBall({super.key, required this.x, required this.y});
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
     double screenWidth = size.width;
     double screenHeight = size.height;
 
-    double currentYPos = ( y * 150 );
-    double currentXPos = ( x * 150 );
-
+    double currentYPos = (y * 300);
+    double currentXPos = (x * 300);
 
     return Stack(
       alignment: Alignment.center,
       children: [
-
         AnimatedPositioned(
-          left: (currentYPos - 25) + ( screenWidth / 2 ),
-          top: (currentXPos - 25) + ( screenHeight / 2 ),
-          curve: Curves.easeInOut, 
-          duration: const Duration(milliseconds: 1000),
-          child: const Ball()
+          left: (currentYPos - 25) + (screenWidth / 2),
+          top: (currentXPos - 25) + (screenHeight / 2),
+          curve: Curves.easeInOut,
+          duration: const Duration(milliseconds: 500),
+          child: const Ball(),
         ),
-
-        
-        Text('''
-      X:$x,
-      Y:$y
-      ''', style: const TextStyle( fontSize: 30 ),),
-
-
+        Text(
+          '''
+            X:$x,
+            Y:$y
+          ''',
+          style: const TextStyle(fontSize: 30),
+        ),
       ],
     );
   }
 }
-
 
 class Ball extends StatelessWidget {
   const Ball({super.key});
@@ -79,8 +71,8 @@ class Ball extends StatelessWidget {
       width: 50,
       height: 50,
       decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(100)
+        color: Colors.amber,
+        borderRadius: BorderRadius.circular(100),
       ),
     );
   }
