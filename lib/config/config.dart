00f29a20acdd6954e8a43a3db53323c09f8a3095
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-15 18:52:13 -0600

    added shared preferences plugin

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -6,3 +6,4 @@ export 'plugins/local_auth_plugin.dart';
 export 'plugins/quick_actions_plugin.dart';
 export 'plugins/share_plugin.dart';
 export 'plugins/app_badge_plugin.dart';
+export 'plugins/shared_preferences_plugin.dart';
