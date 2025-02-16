79b927e9f2f8cf855da2502cabb112fed8689ccd
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-15 18:28:25 -0600

    added full screen ad provider and shows it on ad full screen

--- lib/config/plugins/admob_plugin.dart
+++ lib/config/plugins/admob_plugin.dart
@@ -8,6 +8,11 @@ final adBannerId = Platform.isAndroid
     ? 'ca-app-pub-3940256099942544/9214589741'
     : 'ca-app-pub-3940256099942544/2435281174';
 
+    // ! Replace this test ad unit with your own ad unit.
+final adInterstitialId = Platform.isAndroid
+    ? 'ca-app-pub-3940256099942544/1033173712'
+    : 'ca-app-pub-3940256099942544/4411468910';
+
 
 class AdmobPlugin {
   static Future<void> initialize() async {
@@ -35,4 +40,46 @@ class AdmobPlugin {
     )..load();
   }
 
+
+  static Future<InterstitialAd> loadInterstitialAd() async {
+    Completer<InterstitialAd> completer = Completer();
+
+    InterstitialAd.load(
+        adUnitId: adInterstitialId,
+        request: const AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          // Called when an ad is successfully received.
+          onAdLoaded: (ad) {
+            print('$ad loaded.');
+            // Keep a reference to the ad so you can show it later.
+
+            ad.fullScreenContentCallback = FullScreenContentCallback(
+                // Called when the ad showed the full screen content.
+                onAdShowedFullScreenContent: (ad) {},
+                // Called when an impression occurs on the ad.
+                onAdImpression: (ad) {},
+                // Called when the ad failed to show full screen content.
+                onAdFailedToShowFullScreenContent: (ad, err) {
+                  // Dispose the ad here to free resources.
+                  ad.dispose();
+                },
+                // Called when the ad dismissed full screen content.
+                onAdDismissedFullScreenContent: (ad) {
+                  // Dispose the ad here to free resources.
+                  ad.dispose();
+                },
+                // Called when a click is recorded for an ad.
+                onAdClicked: (ad) {});
+
+            completer.complete(ad);
+          },
+          // Called when an ad request failed.
+          onAdFailedToLoad: (LoadAdError error) {
+            print('InterstitialAd failed to load: $error');
+            completer.completeError(error);
+          },
+        ));
+
+      return completer.future;
+  }
 }
--- lib/presentation/providers/ads/admob_providers.dart
+++ lib/presentation/providers/ads/admob_providers.dart
@@ -4,7 +4,29 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import '../../../config/config.dart';
 
 final adBannerProvider = FutureProvider<BannerAd>((ref) async {
+  // final showAds = ref.watch(showAdsProvider);
+  // if (!showAds) throw 'Ads están bloqueadas';
+
   final ad = await AdmobPlugin.loadBannerAd();
 
   return ad;
 });
+
+final adInterstitialProvider =
+    FutureProvider.autoDispose<InterstitialAd>((ref) async {
+  // final showAds = ref.watch(showAdsProvider);
+  // if (!showAds) throw 'Ads están bloqueadas';
+
+  final ad = await AdmobPlugin.loadInterstitialAd();
+
+  return ad;
+});
+
+// final adRewardedProvider = FutureProvider.autoDispose<RewardedAd>((ref) async {
+//   final showAds = ref.watch(showAdsProvider);
+//   if (!showAds) throw 'Ads están bloqueadas';
+
+//   final ad = await AdmobPlugin.loadRewardedAd();
+
+//   return ad;
+// });
