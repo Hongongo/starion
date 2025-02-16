72588635614653a8ebabb75a0c07575c3f643d0b
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-15 22:56:46 -0600

    added task keys and switch to catch them

--- lib/config/workmanager/callback_dispatcher.dart
+++ lib/config/workmanager/callback_dispatcher.dart
@@ -1,33 +1,31 @@
 import 'package:workmanager/workmanager.dart';
 
-const fetchBackgroundTaskKey = 'com.fernandoherrera.miscelaneos.fetch-background-pokemon';
-const fetchPeriodicBackgroundTaskKey = 'com.fernandoherrera.miscelaneos.fetch-background-pokemon';
+const fetchBackgroundTaskKey = 'com.stardev.starpoint.fetch-background-pokemon';
+const fetchPeriodicBackgroundTaskKey =
+    'com.stardev.starpoint.fetch-periodic-background-pokemon';
 
 // Mandatory if the App is obfuscated or using Flutter 3.1+
-@pragma('vm:entry-point') 
+@pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
-    
-    
-    // switch( task ) {
+    switch (task) {
+      case fetchBackgroundTaskKey:
+        // await loadNextPokemon();
+        print(fetchBackgroundTaskKey);
+        break;
 
-    //   case fetchBackgroundTaskKey:
-    //     await loadNextPokemon();
-    //   break;
+      case fetchPeriodicBackgroundTaskKey:
+        // await loadNextPokemon();
+        print(fetchPeriodicBackgroundTaskKey);
+        break;
 
-    //   case fetchPeriodicBackgroundTaskKey:
-    //      await loadNextPokemon();
-    //   break;
+      case Workmanager.iOSBackgroundTask:
+        print('Workmanager.iOSBackgroundTask');
+        break;
+    }
 
-    //   case Workmanager.iOSBackgroundTask:
-    //     print('Workmanager.iOSBackgroundTask');
-    //   break;
-
-    // }
-    
-    print("Native: called background task: $task"); 
     return true;
-    // //simpleTask will be emitted here.
+    //simpleTask will be emitted here.
     // return Future.value(true);
   });
 }
