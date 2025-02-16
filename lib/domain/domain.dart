9a4a15306e681543b0064c6cbb2a8eae3683511c
Jose Martínez <jo.mtz.133@gmail.com>
2025-02-15 23:27:48 -0600

    added isar local db datasource

--- lib/domain/domain.dart
+++ lib/domain/domain.dart
@@ -1,5 +1,9 @@
 
 
 export 'datasources/pokemons_datasource.dart';
+export 'datasources/local_db_datasource.dart';
+
 export 'entities/pokemon.dart';
+
 export 'repositories/pokemons_repository.dart';
+export 'repositories/local_db_repository.dart';
